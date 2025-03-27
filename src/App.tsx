06f687543da4d407f08293e1ede26974e8020adf
Nicolas Rock <nicolasrpessoa@gmail.com>
2025-03-27 20:09:24 -0300

    Finalizando projeto carrinho-Loja

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/Home/home'
 import { Cart } from './pages/Cart/cart'
 import { Layout } from './components/Layout/Layout'
+import { ProductDetail } from './details/Details'
 
 
 const router = createBrowserRouter([
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "/cart",
         element: <Cart/>
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetail/>
       }
     ]
   }
